a1d61384f91912f6f45979db470996453a842f7b
BTB01 <aymen.boutayebe@gmail.com>
2024-04-23 13:15:01 +0100

    first commit

---
+++ guessinggame.sh
@@ -0,0 +1,42 @@
+
+
+counter= ls|wc -l
+guess(){
+echo"Enter aleat num: "
+read var
+while([var -neq counter]){
+
+}
+
+}
+#!/bin/bash
+
+#Reading number of files in directory
+
+Number_of_files=ls | wc -l
+function files_count()
+{
+  echo "How many files do you guess are there in the current directory:"
+  read response
+  
+  #checking whether guessed number matches with existing files
+  
+  while [ $response -ne $Number_of_files ]
+  do
+     if [ $response -gt $Number_of_files ]
+     then
+      echo "Guessed value is higher than the existing files, guess again:"
+     else
+      echo "Guessed value is lower than the existing files, guess again:"
+     fi
+     read response
+  done
+  echo "Congratulations! You have guessed the exact number of files"
+}
+files_count
+
+
+
+
+
+
